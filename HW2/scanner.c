55badb021112847b53b2eeb2f0508b47557caacd
UandWhatArmy <43387245+UandWhatArmy@users.noreply.github.com>
2023-02-09 11:11:29 -0500

    added output and formating

--- HW2/scanner.c
+++ HW2/scanner.c
@@ -10,18 +10,16 @@
 /*
     TODO:
         - lol idk ???
-
         -Error handling 
             1. Variable does not start with letter. 
             2. Number too long. 
             3. Name too long. 
             4. Invalid symbols. 
-
         -"parsing" thru the code and seperating each thing into tokens
             - we can seperate using a space or "|"
+            - further seperate tokens that may be stuck together such as semicolons and a variable
+            - add a part that skips reading comments
             
-
-
 */
 
 
@@ -35,11 +33,11 @@
 
 //look in Lecture 5 Lexical Analysis
 
-#define  norw      15         /* number of reserved words */
+#define  norw      10         /* number of reserved words */
 #define  imax   32767       /* maximum integer value */
 #define  cmax      11         /* maximum number of chars for idents */
 #define  strmax   256         /* maximum length of strings */
-
+#define  ignoreyml  4          /* length of ignoresym array*/
 
 typedef enum{
     skipsym = 1,identsym, numbersym, plussym, minussym, 
@@ -51,7 +49,10 @@ typedef enum{
 }token_type;
 
 /* list of reserved word names */
-char  *word [ ] = {  "null", "begin", "call", "const", "do", "else", "end", "if", "odd", "procedure", "read", "then", "var", "while", "write"}; 
+const char  *word [ ] = {  "call", "begin", "end", "if", "then", "else", "while", "do", "read", "write"}; 
+
+/* list of ignored symbols */
+const char ignoresym [] = {'\n', '\0', ' ', '\t'};
                          
 /* internal representation  of reserved words */
 // int  wsym [ ] =  { nulsym, beginsym, callsym, constsym, dosym, elsesym, endsym, ifsym, oddsym, procsym, readsym, thensym, varsym, whilesym, writesym};
@@ -65,54 +66,142 @@ char  *word [ ] = {  "null", "begin", "call", "const", "do", "else", "end", "if"
 // ssym['#']=neq;  ssym['<']=lss;  ssym['>']=gtr; 
 // ssym['$']=leq;  ssym['%']=geq;  ssym[';']=semicolon;
 
+
+
 FILE *f;
 
 
-void cpytilspace(char bufferArr[], char charArr[]);
-
+int cpytilspace(char buffer[], char arr[], int arrPointer);
+char* readProgram(int *arrSize);
+int shouldBeIgnored(char c);
 
 int main(int argc, char const *argv[])
 {
 
+
+
     f = fopen(argv[1], "r");
 
+    int arrSize;    
+    int indexPointer = 0;   //keeps track of index we're reading from from array
 
-    char *charArr = (char *) malloc(1 * sizeof(char));
-    int arrSize;
-    for (int i = 0; fscanf(f, "%c", &charArr[i]) > 0; ){    //incrementor in statement
-
-
-        printf("at index %d adding + 1\n",i);
-
-        i++;    //added in statement so it didnt mess up realloc math for some reason
-        charArr = realloc(charArr, sizeof(char) * (i + 1));
-        if(charArr == NULL){
-            free(charArr);
-            return -1; //exit program with error
-        }
+    char *charArr = readProgram(&arrSize);  //process file into one big array
+    char bufferArr[strmax];     //used to help seperate into tokens
+    
+    for (size_t i = 0; (i < arrSize) && (indexPointer < arrSize); i++){
         
-        // printf("%c\n", charArr[i - 1]);
-        
-        arrSize = i;
+        indexPointer = cpytilspace(bufferArr, charArr, indexPointer);
+        printf("buffer holds %s indexptr is %d\n", bufferArr, indexPointer);
 
     }
- 
-    char *bufferArr = malloc(arrSize * sizeof(char));
-    cpytilspace(bufferArr, charArr);
+
+     
+    
+
+
+
 
     //clean up
-    free(bufferArr);
+    // free(bufferArr);
     free(charArr);
     fclose(f);
 
     return 0;
 }
 
-//copys array until it reaches a "space" or bar "|"
-void cpytilspace(char bufferArr[], char charArr[]){
 
-    int i;
-    // while()
+/* returns 1 if symbol should be ignored 0 otherwise */
+int shouldBeIgnored(char c){
+    for (size_t i = 0; i < ignoreyml; i++)
+    {
+        if(c == ignoresym[i])
+            return 1;
+    }
+    
+    return 0;
+}
+
+
+/* reads pl0 input file and puts every character into one huge array and updates the array size
+    returns a char array with everything from file
+*/
+char* readProgram(int *arrSize){
+
+    printf("Source Program: \n");
+
+    char *charArr = (char *) malloc(1 * sizeof(char));
+    // int arrSize;
+    for (int i = 0; fscanf(f, "%c", &charArr[i]) > 0; ){    //incrementor in statement
+
+        printf("%c", charArr[i]); //Displays code, added to comply with program instructions
+
+        // printf("at index %d adding + 1\n",i);
+
+        i++;    //added in statement so it didnt mess up realloc 'math' for some reason
+        charArr = realloc(charArr, sizeof(char) * (i + 1));
+        if(charArr == NULL){
+            free(charArr);
+            printf("Ran out of memory dude. Gotta exit program\n"); //lmao
+            exit(-1); //exit program with error
+        }
+        
+        // printf("%c\n", charArr[i - 1]);
+        
+        *arrSize = i;
+
+    }
+    printf("\n\n");
+
+    return charArr;
+}
+
+
+/*
+    copys array to buffer until it reaches a "space" or bar "|"
+    returns position in charArr
+*/
+int cpytilspace(char buffer[], char arr[], int arrPointer){
+
+    
+    int bufferSize = 0;  //keeps track of size buffer needs to be so we can allocate enough space for it
+    int index = arrPointer; //track where we're working in the array
+    const int init_arrPointer = arrPointer;   //holds where we initially started with the array
+
+    while( !shouldBeIgnored(arr[index]) ){
+
+
+        //checks for comments to ignore and then breaks buffer at spot after comment
+        if (arr[index] == '/' && arr[index + 1] == '*'){
+
+            arrPointer = index;
+            arrPointer += 2;
+
+            while (arr[arrPointer] != '*' && arr[arrPointer + 1] != '/'){
+                arrPointer++;
+            }
+            
+            arrPointer++;
+            index = arrPointer;
+            
+            break;
+
+        }
+
+        bufferSize++;
+        index++;
+    }
+    
+
+    // printf("buffersize %d\n", bufferSize);
+
+    //copying the valid word chunk from arr to buffer
+    for(int k = 0; k < bufferSize; k++){
+        buffer[k] = arr[init_arrPointer + k];
+    }
+    buffer[bufferSize] = '\0';  //always add terminator to strs in c
+
+    // printf("functionm buffer holds %s index is %d\n", buffer, index);
+    return index + 1;   //return where we will continue workin with array
 
 
 }
